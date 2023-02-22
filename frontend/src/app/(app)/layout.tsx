a89930a21e0b91122c9372258c1a1ef0f3bd52ee
Andh <andhikabukh@gmail.com>
2023-02-22 19:13:01 +0700

    Added App Layout System

---
+++ frontend/src/app/(app)/layout.tsx
@@ -0,0 +1,22 @@
+import Sidebar from "@/component/sidebar";
+
+export default function App({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="flex min-h-screen min-w-full">
+			<Sidebar />
+
+			<div className="flex flex-1 flex-col">
+				<div className="py-6 px-5">
+					<h1 className="font-medium text-content-60">
+						Bootcamp&nbsp;&nbsp;/&nbsp;&nbsp;inosoft-bootcamp&nbsp;&nbsp;/&nbsp;&nbsp;
+						<span className="text-content-100">Batch#14</span>
+					</h1>
+				</div>
+
+				<main className="max-[1050px]:rounded-tl-none flex flex-1 items-center justify-center rounded-tl-xl bg-background">
+					{children}
+				</main>
+			</div>
+		</div>
+	);
+}
